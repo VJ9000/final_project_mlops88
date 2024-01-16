c5f732f26ead74319d8587a51e52cb7d5b1d7ed7
MDLM94 <mathias.dal@gmail.com>
2024-01-16 10:24:01 +0100

    small changes in make_dataset.py

--- mlops88_ezCNNs/data/make_dataset.py
+++ mlops88_ezCNNs/data/make_dataset.py
@@ -1,3 +1,14 @@
+import os
+
+try:
+    import dvc
+except ImportError:
+    print("dvc is not installed")
+
+# dvc needs to be in the path
+os.system("dvc pull")
+
+
 if __name__ == '__main__':
     # Get the data and process it
     pass
