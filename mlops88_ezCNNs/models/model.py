25572c5d53be1541f37222a1fc48ed0a25b4e557
VJ <vitale.joregnsen@gmail.com>
2024-01-18 16:34:43 +0100

    added functionality to use wandb for training the model

--- mlops88_ezCNNs/models/model.py
+++ mlops88_ezCNNs/models/model.py
@@ -4,6 +4,10 @@ from torch import nn
 import timm
 from torchmetrics.classification import Accuracy
 import pytorch_lightning as pl 
+from torchvision.utils import make_grid
+import wandb
+from PIL import Image, ImageDraw, ImageFont
+from torchvision import transforms
 
 class MyTimmNet(pl.LightningModule):
     def __init__(self, train_loader, test_loader, class_names, model_name='resnet18', learning_rate=0.001):
@@ -19,7 +23,6 @@ class MyTimmNet(pl.LightningModule):
         self.class_names = class_names
         self.test_results = {'outputs': [], 'labels': []}
         self.learning_rate = learning_rate
-
 
     def forward(self, x):
         return self.model(x)
@@ -53,7 +56,6 @@ class MyTimmNet(pl.LightningModule):
         acc = self.accuracy(outputs, labels)
         return acc
 
-
     def on_test_epoch_end(self):
         all_outputs = torch.cat(self.test_results['outputs'])
         all_labels = torch.cat(self.test_results['labels'])
@@ -68,6 +70,37 @@ class MyTimmNet(pl.LightningModule):
                 class_accuracies.append(class_accuracy)
 
                 print(f'Class {class_name} Accuracy: {class_accuracy * 100:.2f}%')
+                wandb.log({f'Class_{class_name}_Accuracy': class_accuracy * 100})
+
 
         avg_acc = sum(class_accuracies) / len(class_accuracies)
         print(f'Average Test Accuracy: {avg_acc * 100:.2f}%')
+        wandb.log({'Average_Test_Accuracy': avg_acc * 100})
+        
+        images, labels = next(iter(self.test_loader))
+        predictions = torch.argmax(self(images), dim=1)
+        class_names = [self.class_names[i] for i in predictions]
+
+        grid = make_grid(images, nrow=4, normalize=True)
+        grid_with_text = self.add_text_to_image_grid(grid, class_names)
+
+        wandb.log({"Test Images": [wandb.Image(grid_with_text, caption="Test Images")]})
+    
+    def add_text_to_image_grid(self, grid, predictions):
+        # Convert the PyTorch tensor to a PIL image
+        pil_image = transforms.ToPILImage()(grid)
+
+        # Create a drawing object
+        draw = ImageDraw.Draw(pil_image)
+        
+        # Choose a font (you may need to adjust the path)
+        font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"  
+        font = ImageFont.truetype(font_path, size=14)
+
+        # Add predictions as white text to the images
+        for i, prediction in enumerate(predictions):
+            x = (i % 4) * 128 + 10
+            y = (i // 4) * 128 + 10
+            draw.text((x, y), prediction, fill="blue", font=font)
+
+        return pil_image
--- mlops88_ezCNNs/train_model.py
+++ mlops88_ezCNNs/train_model.py
@@ -3,14 +3,27 @@ from models.model import MyTimmNet
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 import hydra
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+
+wandb_logger = WandbLogger(log_model="all")
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config_1")
 def main(cfg):
+    
+    wandb.init(
+    project="Testing with RESNET18",
+    )
+    
+    
+    
     train_loader,test_loader, class_names = get_data_loaders(cfg.paths.all_images ,cfg.params.batch_size)
     model = MyTimmNet(train_loader, test_loader, class_names, model_name=cfg.models.name, learning_rate=cfg.params.lr)
-    trainer = pl.Trainer(max_epochs=cfg.params.epoch_count)  
+    trainer = pl.Trainer(logger=wandb_logger,max_epochs=cfg.params.epoch_count)  
     trainer.fit(model, train_loader, test_loader)
     trainer.test(model, test_loader)
+    
+    wandb.finish()
 
 if __name__ == "__main__":
     main()
